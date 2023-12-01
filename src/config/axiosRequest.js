7dda4e99acaef30db43368d5e42bf96a5c8160b3
dantr <huynhngdantr@gmail.com>
2023-12-01 19:11:19 +0700

    cookies get

--- src/config/axiosRequest.js
+++ src/config/axiosRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 const axiosRequest = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
-  // withCredentials: true, // allow to get cookies
+  withCredentials: true, // allow to get cookies
 });
 export default axiosRequest;
