1dedb0e186af114ae84a6b2887521393bf48f57d
dantr <huynhngdantr@gmail.com>
2023-12-02 01:38:32 +0700

    upload done and handle error , not handle display and loading

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,66 @@
-import React from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import React, { useState } from "react";
+import { app } from "../firebase/firebase";
 
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({ imageUrls: [] });
+  const [imageUploadFileError, setImageUploadFileError] = useState(false);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+        })
+        .catch((error) => {
+          setImageUploadFileError("image upload failed");
+        });
+    } else {
+      setImageUploadFileError("you can only upload  6 images");
+    }
+  };
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+
+      const fileName = new Date().getTime() + file.name;
+
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(
+            "🚀 ~ file: CreateListing.jsx:44 ~ returnnewPromise ~ progress:",
+            progress + "%"
+          );
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+            resolve(downloadUrl);
+          });
+        }
+      );
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -115,6 +175,7 @@ export default function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               name=""
@@ -122,13 +183,21 @@ export default function CreateListing() {
               accept="image/*"
               multiple
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded hover:shadow-lg disabled:opacity-70 uppercase">
+            <button
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 border border-green-700 rounded hover:shadow-lg disabled:opacity-70 uppercase"
+            >
               Upload
             </button>
           </div>
+
           <button className="uppercase text-white bg-slate-600 p-3  rounded-lg hover:opacity-90 disabled:opacity-80">
             create listing
           </button>
+          <p className="text-red-600">
+            {imageUploadFileError && imageUploadFileError}
+          </p>
         </div>
       </form>
     </main>
