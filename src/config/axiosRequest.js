425f5cd9c7b846aceb3fd340a22d81b5d61558e7
dantr <huynhngdantr@gmail.com>
2023-12-01 18:39:19 +0700

    config cookies for request

--- src/config/axiosRequest.js
+++ src/config/axiosRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-const BASE_URL =
-  process.env.REACT_APP_API_URL || "https://api-realestate-blue.vercel.app";
-console.log(process.env.REACT_APP_API_URL);
+const BASE_URL = process.env.REACT_APP_API_URL;
+
 const axiosRequest = axios.create({
   baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true, // allow to get cookies
 });
 export default axiosRequest;
